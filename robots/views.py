56b734212353722e7a5bd8969f92239bea50c710
Денис Набатов <nabatoffdenis@yandex.ru>
2023-10-05 05:30:48 +0300

    second task

--- robots/views.py
+++ robots/views.py
@@ -14,8 +14,7 @@ from .models import Robot, RobotModel
 @method_decorator(csrf_exempt, name='dispatch')
 class NewRobot(View):
 
-    @staticmethod
-    def post(request):
+    def post(self, request):
         data = json.loads(request.body.decode('utf-8'))
         robot_model_name = data.pop('model')
         try:
@@ -33,3 +32,44 @@ class NewRobot(View):
         )
 
         return JsonResponse(status=201, data={'id': robot.id})
+
+    def get(self, request):
+        robots = Robot.objects.filter(
+            created__gte=datetime.now() - timedelta(days=7)
+        ).values(
+            'model__name', 'version'
+        ).annotate(
+            count=Count('id')
+        ).order_by('model__name')
+
+        robots_data = {}
+        for robot in robots:
+            robot_name = robot['model__name']
+            versions = {'version': robot['version'], 'count': robot['count']}
+            if not robots_data.get(robot_name):
+                robots_data[robot_name] = [versions]
+                continue
+            robots_data[robot_name].append(versions)
+
+        return self.create_excel_file(robots_data)
+
+    @staticmethod
+    def create_excel_file(robots_data):
+        response = HttpResponse(
+            content_type=f'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        )
+        response['Content-Disposition'] = 'attachment; filename={date}-robots.xlsx'.format(
+            date=datetime.now().strftime('%Y-%m-%d'),
+        )
+        wb = openpyxl.Workbook()
+        list_ind = 0
+        column_names = ['Модель', 'Версия', 'Количество за неделю']
+        for model, versions in robots_data.items():
+            excel_list = wb.create_sheet(model, list_ind)
+            excel_list.append(column_names)
+            for version_data in versions:
+                row = [model, version_data['version'], version_data['count']]
+                excel_list.append(row)
+            list_ind += 1
+        wb.save(response)
+        return response
